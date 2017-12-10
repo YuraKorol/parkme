a20a87bd3f068fb795ca7d7eeb3831ca370e424f
YuraKorol <yrakorol200012@gmail.com>
2017-12-10 23:44:40 +0200

    add code

---
+++ app/src/main/java/com/example/yura/parkme/ActivityTwo.java
@@ -0,0 +1,32 @@
+package com.example.yura.parkme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
+
+    Button code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        code = (Button) findViewById(R.id.code);
+        code.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.code) {
+            Intent intent = new Intent(this, SlideMenu.class);
+            startActivity(intent);
+
+        } else {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/yura/parkme/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.yura.parkme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btnActTwo) {
+            Intent intent = new Intent(this, ActivityTwo.class);
+            startActivity(intent);
+
+        } else {
+        }
+    }
+}
